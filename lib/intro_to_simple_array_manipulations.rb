6d2dd8ee6854ab47a3b1617809918fe0107576cb
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-06-27 16:36:23 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,16 @@
+def colors(array)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"
+  next_color = "violet"
+  colors_in_the_rainbow.push(next_color)
+end
+
+def boroughs(array)
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  bouroughs_in_nyc.unshift("Staten Island")
+end
+
+def continents(array)
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  Antarctica = continents.pop
+end
+
